e990de3ff26533cfc79f915ace5b6f8bacf3340b
gilimiz <gilimizrchi@gmail.com>
2026-07-16 13:46:33 +0300

    first steps

---
+++ Project12-Final_Project_Part_2/final_project/final_project.ino
@@ -0,0 +1,39 @@
+#include <MD_PWM.h>
+
+const int rotaryPin = A0;
+const uint8_t pwmPin = 10;     // PWM-capable pin connected to the LED through your wiring
+const uint16_t PWM_FREQ = 50; // Hz
+
+MD_PWM pwm(pwmPin);
+
+void setup()
+{
+  Serial.begin(9600);
+
+  if (!pwm.begin(PWM_FREQ))
+  {
+    Serial.println("Failed to initialize PWM");
+  }
+}
+
+void loop()
+{
+  static int lastRotaryValue = -1;
+
+  int rotaryValue = analogRead(rotaryPin);
+
+  if (rotaryValue != lastRotaryValue)
+  {
+    uint8_t duty = map(rotaryValue, 0, 1023, 0, 255);
+    pwm.write(duty);
+
+    Serial.print("Rotary: ");
+    Serial.print(rotaryValue);
+    Serial.print(" -> PWM duty: ");
+    Serial.println(duty);
+
+    lastRotaryValue = rotaryValue;
+  }
+
+  delay(50);
+}
